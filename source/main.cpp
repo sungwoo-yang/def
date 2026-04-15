ef3dee4411d5f03e2fdc85a6a752579d27fc3e76
sungwoo-yang <sungwoo.yang@digipen.edu>
2026-04-16 02:48:14 +0900

    update opengl starter

---
+++ source/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * \file
+ * \author Rudy Castan
+ * \date 2024 Fall
+ * \copyright DigiPen Institute of Technology
+ */
+
+#include <iostream>
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+    std::cout << "Hello World!\nRudy is here\n";
+
+    return 0;
+}
